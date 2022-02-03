dbdc43cfe5987e4ebf183586ff4945b69610eb7b
yuanita7149 <yuanitafebyanty@gmail.com>
2022-02-03 12:48:06 +0700

    first commit

---
+++ application/controllers/Hello.php
@@ -0,0 +1,13 @@
+<?php
+class Hello extends CI_Controller{
+
+    function index(){
+        echo "Hello Word";
+    }
+
+    function show(){
+        echo "I Make The Word Better Place.";
+    }
+
+
+}    
